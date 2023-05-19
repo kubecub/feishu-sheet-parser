ec8f465775f1dc5a8bd0a666ee3be3b125baf7da
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-05-19 16:00:30 +0800

    feat(main): adding git hooks
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

--- internal/controllers/Send.go
+++ internal/controllers/Send.go
@@ -1,14 +1,10 @@
 package controllers
 
 import (
-	"feishu_sheets_api/model"
 	"net/http"
 )
 
 var (
-	feiShuConfig = &model.FeiShuConfig{}
-	feiShuData   = &model.FeiShuData{}
-	pushData     = &model.PushData{}
 )
 
 func SendDataHandle(w http.ResponseWriter, req *http.Request) {
